2b64a624097b7e66e6f57a24190df7d9f0fa7fb6
Alexandre Penasso Teixeira <alexandre@MacBook-Pro-de-Alexandre.local>
2016-09-29 15:46:26 -0300

    Sample Auth to EAP220

---
+++ README.md
@@ -0,0 +1,15 @@
+# eap220
+
+Sample Authetication to extend portal;
+
+Please Update your EAP220:
+http://www.tp-link.com/en/download/EAP220.html
+EAP Controller_V2.4.4,EAP220(EU)_V1_160401.
+
+Install the wampserver windows:
+https://sourceforge.net/projects/wampserver/
+
+If you any question, please send email to alexandre at ktudu dot com.
+
+Good Luck !
+
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+  ob_start();
+  session_start();
+
+  if (isset($_GET['cid'])) {
+      $cid =  $_GET['cid'];
+  }else{
+      $cid = 'null';
+  }
+
+  if (isset($_GET['ap'])) {
+      $ap =  $_GET['ap'];
+  }else{
+      $ap = 'null';
+  }
+
+  if (isset($_GET['ssid'])) {
+      $ssid =  $_GET['ssid'];
+  }else{
+      $ssid = 'null';
+  }
+
+  if (isset($_GET['t'])) {
+      $t =  $_GET['t'];
+  }else{
+      $t = 'null';
+  }
+
+  if (isset($_GET['rid'])) {
+      $rid =  $_GET['rid'];
+  }else{
+      $rid = 'null';
+  }
+  if (isset($_GET['site'])) {
+      $site =  $_GET['site'];
+  }else{
+      $site = 'null';
+  }
+
+  $time = 60;// One hour; 
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_COOKIEJAR, "/tmp/cookieFileName");
+  curl_setopt($ch, CURLOPT_URL, "https://127.0.0.1:8043/login");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, "name=admin&password=pmlsystem");
+
+  ob_start();      // prevent any output
+
+  $login = curl_exec ($ch); // execute the curl command
+  ob_end_clean();
+
+  curl_setopt($ch, CURLOPT_URL,"https://127.0.0.1:8043/extportal/Default/auth");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, 'cid='.$cid.'&ap='.$ap.'&ssid='.$ssid.'&t='.$t.'&rid='.$rid.'&site='.$site.'&time='.$time);
+
+  ob_start();
+  $portal = curl_exec ($ch);
+  ob_end_clean();
+
+
+  curl_setopt($ch, CURLOPT_URL,"https://127.0.0.1:8043/logout");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_POST, true);
+
+  ob_start();
+  $logout = curl_exec ($ch);
+  //echo "string2<br/>";
+  //echo $buf.'<br/>';
+  curl_close ($ch);
+  ob_end_clean();
+
+  header("Location: http://www.ktudu.com", true, 301);
+  exit();
+?>
